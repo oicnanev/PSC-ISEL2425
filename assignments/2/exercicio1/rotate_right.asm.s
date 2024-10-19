7371adbed22be2b21ed6401ad6e5a6cc2d79f535
oicnanev <voicnanev@proton.me>
2024-10-19 09:32:41 +0100

    better comments in rotate_right from exercicio1

--- assignments/2/exercicio1/rotate_right.asm.s
+++ assignments/2/exercicio1/rotate_right.asm.s
@@ -7,20 +7,20 @@ rotate_right:
     # n - número de posições a deslocar
 
     # Carregar os parâmetros
-    movq %rdi, %r8                # value
-    movq %rsi, %rcx               # n 
+    movq %rdi, %r8                # endereço de value - preservado em r8
+    movq %rsi, %rcx               # endereço n - preservado em rcx
 
     # Carregar os valores de 64 bits
-    movq (%r8), %rax              # valor baixo
-    movq 8(%r8), %rdx             # valor alto
+    movq (%r8), %rax              # parte baixa apontada por r8 para rax
+    movq 8(%r8), %rdx             # parte alta apontada por r8 + 8 para rdx
 
     # Deslocar / rodar os bits
-    shrd %cl, %rdx, %rax          # deslocar valor baixo
-    shrd %cl, %rax, %rdx          # deslocar valor alto
+    shrd %cl, %rdx, %rax          # deslocar para a dir n bits do rdx para rax
+    shrd %cl, %rax, %rdx          # deslocar para a dir n bits do rax para rdx
 
-    # Armazenar o resultado para retorno
-    movq %rax, (%r8)              # guarda o valor baixo
-    movq %rdx, 8(%r8)             # guarda o valor alto
+    # Armazenar o resultado
+    movq %rax, (%r8)              # guarda a parte baixa no endereço apontado por r8
+    movq %rdx, 8(%r8)             # guarda a parte alta no endereço apontado por r8 + 8
 
     ret
 

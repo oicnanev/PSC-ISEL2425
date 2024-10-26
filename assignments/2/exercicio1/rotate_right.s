153a1b3fbef8525aec73d2f97b1a38361528da8b
oicnanev <voicnanev@proton.me>
2024-10-26 16:25:00 +0100

    ex1 as2 - miss testing

--- assignments/2/exercicio1/rotate_right.s
+++ assignments/2/exercicio1/rotate_right.s
@@ -32,14 +32,23 @@ main_algorithm:
     movq (%r8), %rax              # parte baixa apontada por r8 para rax
     movq 8(%r8), %rdx             # parte alta apontada por r8 + 8 para rdx
 
-    # Preservar copia da parte baixa
-    movq %rax, %r9
-    # TODO: fazer máscara com n a 1s
-
     # Deslocar / rodar os bits
+    movq %rax, %r9                # preservar cópia da parte baixa em r9
     shrd %rsi, %rdx, %rax         # deslocar para a dir n bits do rdx para rax
     shrd %rsi, %rax, %rdx         # deslocar para a dir n bits do rax para rdx
-    # TODO: adicionar a parte em falta a %rdx delocado 32 - n para a esquerda
+    # TODO: adicionar a parte em falta a %rdx delocado 64 - n para a esquerda
+    movq %10, %rsi                # copiar n (%rsi) para %r10
+    sub  $64, %r10                # n - 64
+    ror  %r10, %r9                # rodar os bits da parte baixa 64 - n
+
+    # criar mascara
+    movq $-1, %r11                # todos os bits de %r11 a 1
+    shl  $1, %r10                 # deslocar n bits para a esquerda
+    sub  $1, %r10                 # ajustar para criar mascara correta
+    not  %r10                     # inverter para ter máscara correta
+    and  %r11, %r10               # meter bits que não interessam a 0
+    and  %r10, %r9                # meter bits que não interessam a 0
+    or   %r9, %r8                 # copiar a parte em falta para %r8
 
     # Armazenar o resultado
     movq %rax, (%r8)              # guarda a parte baixa no endereço apontado por r8

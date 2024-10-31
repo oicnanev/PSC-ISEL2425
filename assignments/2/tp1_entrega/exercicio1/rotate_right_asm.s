8574d02910611e58965d348c61bb81c59d3d392c
oicnanev <voicnanev@proton.me>
2024-10-31 12:39:44 +0000

    ex1-assign2 OK

--- assignments/2/tp1_entrega/exercicio1/rotate_right_asm.s
+++ assignments/2/tp1_entrega/exercicio1/rotate_right_asm.s
@@ -6,50 +6,40 @@
 # - rdi: Ponteiro para o array de 2x64 bits (value)
 # - esi: Número de posições para deslocar (n)
 rotate_right:
-    test %esi, %esi
-    ret
+    # Guardar os registos de propósito geral
+    push    %rbx
+    push    %rcx
+    push    %rdx
 
-    # Salva os registos de propósito geral
-    push %rbx
-    push %rcx
-    push %rdx
+    # Carregar os dois valores de 64 bits
+    mov     (%rdi), %rax
+    mov     8(%rdi), %rbx
 
-    # Carrega os dois valores de 64 bits
-    mov (%rdi), %rax
-    mov 8(%rdi), %rbx
-
-    # Calcula a rotação
-    mov %esi, %ecx
-    and $127, %ecx  # Limita o número de posições a 127
+    # Truncar a rotação
+    mov     %esi, %ecx
+    and     $127, %ecx          # Limita o número de posições a 127
 
     # Se a rotação for maior ou igual a 64, trocar os registos
-    cmp $64, %ecx
-    jb rotate
+    cmp     $64, %ecx
+    jb      rotate
 
     # Troca os registos e ajusta a rotação
-    xchg %rax, %rbx
-    sub $64, %ecx
+    xchg    %rax, %rbx
+    sub     $64, %ecx
 
 rotate:
     # Realizar a rotação
-    mov %rax, %rdx
-    shrd %cl, %rbx, %rax
-    shrd %cl, %rdx, %rbx
-
-    # Restaura os bits rotacionados
-    mov %rax, %rdx
-    shr %cl, %rdx
-    or %rdx, %rbx
-    shld %cl, %rax, %rbx
+    mov     %rax, %rdx          # preservar parte baixa
+    shrd    %cl, %rbx, %rax     # rotação da parte alta para a baixa
+    shrd    %cl, %rdx, %rbx     # rotação da parte baixa para a alta
 
     # Colocar o valor deslocado de volta para a array
-    mov %rax, (%rdi)
-    mov %rbx, 8(%rdi)
+    mov     %rax, (%rdi)
+    mov     %rbx, 8(%rdi)
 
     # Restaurar os registos de propósito geral
-    pop %rdx
-    pop %rcx
-    pop %rbx
+    pop     %rdx
+    pop     %rcx
+    pop     %rbx
 
     ret
-

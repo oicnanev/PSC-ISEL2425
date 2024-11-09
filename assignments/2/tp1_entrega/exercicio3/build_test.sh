8e7aafd9f53fb9e6c183dcb7d3976414800e9679
oicnanev <voicnanev@proton.me>
2024-11-09 21:37:37 +0000

    add test for info.valid = 0 in tp1-ex3

---
+++ assignments/2/tp1_entrega/exercicio3/build_test.sh
@@ -0,0 +1,2 @@
+gcc -g -Wall test_get_val_ptr.c get_val_ptr_asm.s -o test_get_val_ptr
+
--- assignments/2/tp1_entrega/exercicio3/test_get_val_ptr.c
+++ assignments/2/tp1_entrega/exercicio3/test_get_val_ptr.c
@@ -25,23 +25,26 @@ short vals2[] = {5, 15, 25};
 data data1 = {0b000001, 5, vals1};  // flags = 0b000001, length = 5
 data data2 = {0b000010, 3, vals2};  // flags = 0b000010, length = 3
     
-// Criar estrutura info com array de ponteiros para 'data'
+// Criar struct info com array de ponteiros para 'data'
 data *data_arr[] = {&data1, &data2};
-info items[1] = {
-    { 1.0, data_arr, 1 }  // ref = 1.0, dados válidos e apontando para 'data_arr'
+info items[2] = {
+    { 1.0, data_arr, 1 },  // ref = 1.0, dados válidos e apontando para 'data_arr'
+    { 2.0, data_arr, 0 }  // ref = 2.0, dados invalidos e apontados para 'data_arr'
 };
 
 
+
 int main() {
-    // Parâmetros para testar a função
+    // Parâmetros globais a todos os testes
     size_t item_idx = 0;
-    size_t data_idx = 0;
-    size_t val_idx = 2;     // Acessar o índice 2 de vals (valor = 30)
+    size_t data_idx = 0;    
     short mask = 0b000001;  // Testar o bit 0 das flags (que é 1 em data1)
     
-    // Chamar função assembly
+    // Teste com valor válido
+    size_t val_idx = 2;     // índice 2 de vals (valor = 30)
     short *val_ptr = get_val_ptr(items, item_idx, data_idx, val_idx, mask);
     
+    printf("Teste com valor 30\t\t");
     if (val_ptr != NULL) {
         printf("Valor encontrado: %d\n", *val_ptr);  // Deve imprimir 30
     } else {
@@ -49,6 +52,7 @@ int main() {
     }
     
     // Teste com um valor inválido
+    printf("Teste com indice fora do limite\t");
     val_idx = 10;  // Índice fora do limite, deve retornar NULL
     val_ptr = get_val_ptr(items, item_idx, data_idx, val_idx, mask);
     
@@ -56,6 +60,18 @@ int main() {
         printf("Valor encontrado: %d\n", *val_ptr);
     } else {
         printf("Valor não encontrado ou condição falhou.\n");  // Esperado: "Valor não encontrado"
+    }
+
+    // Teste com info not valid
+    printf("Teste com info.valid = 0\t");
+    val_idx = 1;
+    item_idx = 1;
+    val_ptr = get_val_ptr(items, item_idx, data_idx, val_idx, mask);  // info.valid = 0
+
+    if (val_ptr != NULL) {
+        printf("valor encontrado: %d\n", *val_ptr);
+    } else {
+        printf("Valor não encontrado ou condição falhou\n");  // Esperado "Condição falhou"
     }
 
     return 0;
